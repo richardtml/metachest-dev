813a16d55bc53ebeba0b0a0dba108e250eb21526
bereml <bereml91@gmail.com>
2024-05-13 19:12:36 -0600

    Add nb to generate complete distro

--- common.py
+++ common.py
@@ -1,5 +1,6 @@
 
 import tomllib
+import tomli_w
 
 
 MIN_AGE, MAX_AGE = 10, 80
@@ -62,6 +63,50 @@ PADCHEST_PATHOLOGIES = [
 ]
 
 
+MTRN = [
+    'effusion',
+    'lung_opacity',
+    'atelectasis',
+    'infiltration',
+    'nodule',
+    'mass',
+    'pleural_thickening'
+]
+
+MVAL = [
+    'emphysema',
+    'fibrosis',
+    'hernia'
+]
+
+MTST = [
+    'cardiomegaly',
+    'edema',
+    'pneumothorax',
+    'consolidation',
+    'pneumonia'
+]
+
+MCLASSES = {'mtrn': MTRN, 'mval': MVAL, 'mtst': MTST}
+
+
+def filter_msets(df, filter_df, mclasses):
+    mset_exclude = {
+        'mtrn': mclasses['mval'] + mclasses['mtst'],
+        'mval': mclasses['mtst'],
+        'mtst': mclasses['mval'],
+    }
+    for mset, classes in mset_exclude.items():
+        exclude = df[classes].any(axis=1)
+        filter_df.loc[exclude, mset] = 0
+    return filter_df
+
+
 def read_toml(path):
     with open(path, 'rb') as f:
         return tomllib.load(f)
+
+
+def save_toml(path, data):
+    with open(path, mode='wb') as f:
+        tomli_w.dump(data, f)
